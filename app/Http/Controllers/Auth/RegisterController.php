59f2dcce5352af3ad60843c534270f94dd5c43ca
rmansilla8 <r.mansilla05@gmail.com>
2018-07-23 18:49:55 -0600

    Se cambió el nombre del namespace

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace Sisteg\Http\Controllers\Auth;
 
-use App\User;
-use App\Http\Controllers\Controller;
+use Sisteg\User;
+use Sisteg\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
@@ -59,7 +59,7 @@ class RegisterController extends Controller
      * Create a new user instance after a valid registration.
      *
      * @param  array  $data
-     * @return \App\User
+     * @return \Sisteg\User
      */
     protected function create(array $data)
     {
--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace Sisteg\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use Sisteg\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
 class ResetPasswordController extends Controller
