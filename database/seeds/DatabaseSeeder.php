bddb05e7fdb25c006022d44cd7561e67298bda66
Roberto Mansilla <r.mansilla05@gmail.com>
2020-09-03 12:07:01 -0600

    Eliminando Seeders genéricos

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,36 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            UsersSeeder::class,
-            //PasswordResetsSeeder::class,
-            CivilStatesSeeder::class,
-            LanguagesSeeder::class,
-            LanguageDomainsSeeder::class,
-            TitlesSeeder::class,
-            EthnicCommunitiesSeeder::class,
-            PlanSeeder::class,
-            DepartmentsSeeder::class,
-            MunicipalitiesSeeder::class,
-            ClassificationsSeeder::class,
-            ModalitiesSeeder::class,
-            AreasSeeder::class,
-            LevelsSeeder::class,
-            WorkingDaysSeeder::class,
-            AffiliateStatesSeeder::class,
-            PositionsSeeder::class,
-            CommitteeLevelsSeeder::class,
-            EmployeeTypesSeeder::class,
-            ContractsSeeder::class,
-            GendersSeeder::class,
-            SchoolDistrictsSeeder::class,
-            SchoolsSeeder::class,
-            PeopleSeeder::class,
-            EmployeesSeeder::class,
-            AffiliatesSeeder::class,
-            AdministrativeEmployeesSeeder::class,
-            FeeTypesSeeder::class,
-            FeesSeeder::class,
-            //
+            
         ]);
     }
 }
