cbc7bad94b0eb29601d2849dcacc913328e4874c
lesdith <lesdith@gmail.com>
2020-09-07 22:10:54 -0600

    WorkStatusSeeder :)

---
+++ database/seeds/WorkStatusTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class WorkStatusTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('work_status')->insert([
+            [
+                'description'   =>  'Activo'
+            ],
+        ]);
+    }
+}
